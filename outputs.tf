52c41e29fbc533a81c2b63770a348b4f8e542ff3
NitinBisht28 <nitin.bisht.connect@gmail.com>
2026-02-21 20:01:59 +0530

    updated outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,8 @@
 output "ec2_public_ip" {
     value = [
-        for instance in aws_instance.terra-ec2 : instance.public_ip
+        for instance in aws_instance.terra-ec2 : {
+            name = instance.tags.Name
+            public_ip = instance.public_ip
+            }
     ]
-  
 }
